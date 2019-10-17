672de6e29ba9e0b98053b4b2030e64dee6ffb135
Georgy Chabukiani <georgy.chabukiani@globallogic.com>
2019-10-17 12:24:18 +0300

    net: enable feature ipsec_tunnels
    Jira id: ADM-3866.
    Tests require enabled feature ipsec_tunnels.
    
    Signed-off-by: Georgy Chabukiani <georgy.chabukiani@globallogic.com>
    Change-Id: I6332d3ab8c6509f379980d447b0c75c704ee42ae

--- DeviceCommon.mk
+++ DeviceCommon.mk
@@ -80,6 +80,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Used to embed a map in an activity view
