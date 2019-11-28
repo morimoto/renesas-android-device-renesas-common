46bc2b5c03e6d23c3a51c956b89e0b42f5776c40
dstakhnik <denys.stakhnik@globallogic.com>
2019-11-28 14:46:26 +0200

    Disable vulkan
    Signed-off-by: dstakhnik <denys.stakhnik@globallogic.com>
    Change-Id: I6b1277b2e6cfe5faadc08073dd54f745de9ac652
    Jira-ID: ADM-4317

--- DeviceCommon.mk
+++ DeviceCommon.mk
@@ -375,12 +375,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     irqbalance
 
-# vulkan features
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
-
 # ----------------------------------------------------------------------
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 
