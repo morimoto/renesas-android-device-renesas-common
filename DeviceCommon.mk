e506710d985c2217099b58a43043681492970696
Oleh Cherpak <oleh.cherpak@globallogic.com>
2020-01-29 16:37:30 +0200

    DeviceCommon.mk: Remove Wifi VendorHAL
    Jira-Id: ADM-4308
    
    Signed-off-by: Oleh Cherpak <oleh.cherpak@globallogic.com>
    Change-Id: I8b07f851a9d9d1530562695810947bf8cdf399f2

--- DeviceCommon.mk
+++ DeviceCommon.mk
@@ -298,12 +298,9 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    libwifi-hal \
     android.hardware.wifi.supplicant@1.0-service \
     android.hardware.wifi.supplicant@1.1-service \
     android.hardware.wifi.supplicant@1.2-service \
-    android.hardware.wifi@1.0-service \
-    android.hardware.wifi@1.0-service-lib \
     hostapd \
     wlutil \
     wificond \
