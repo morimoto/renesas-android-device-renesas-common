606ccef501e080954622e6f688ea5db8a21c819a
Vitaliy Vasylskyy <vitaliy.vasylskyy@globallogic.com>
2020-03-30 11:05:46 +0300

    rcar_ion: Set proper ion header
    Previous headers were unused by build system,
    so they are removed. New headers has to be used
    by user space apps to send the RCAR_ION_CUSTOM
    ioctl. Apps should add following line into their
    Android.mk file:
    
    LOCAL_C_INCLUDES := TARGET_BOARD_KERNEL_HEADERS
    
    or in case of Android.bp:
    
        header_libs: [
            "device_kernel_headers",
        ],
    
    and then include the following header :
    #include <uapi/linux/rcar_ion.h>
    
    Jira-Id: ADM-4623
    Signed-off-by: Vitaliy Vasylskyy <vitaliy.vasylskyy@globallogic.com>
    Change-Id: I2d5097856d12f848a439f154363d61a64251a4b0
    (cherry picked from commit f11770f44f9d9de32918675e2dc205c102af4de7)

--- kernel-headers/uapi/linux/rcar_ion.h
+++ kernel-headers/uapi/linux/rcar_ion.h
@@ -16,46 +16,19 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef _UAPI_LINUX_ION_H
-#define _UAPI_LINUX_ION_H
-#include <linux/ioctl.h>
-#include <linux/types.h>
-
-// # error "device/renesas/common/kernel-headers/linux/ion.h"
-enum ion_heap_type {
-  ION_HEAP_TYPE_SYSTEM,
-  ION_HEAP_TYPE_SYSTEM_CONTIG,
-  ION_HEAP_TYPE_CARVEOUT,
-  ION_HEAP_TYPE_CHUNK,
-  ION_HEAP_TYPE_DMA,
-  ION_HEAP_TYPE_CUSTOM,
+#ifndef __UAPI_RCAR_ION_H__
+#define __UAPI_RCAR_ION_H__
+struct ion_custom_data {
+  unsigned int cmd;
+  unsigned long arg;
 };
-#define ION_NUM_HEAP_IDS (sizeof(unsigned int) * 8)
-#define ION_FLAG_CACHED 1
-struct ion_allocation_data {
-  __u64 len;
-  __u32 heap_id_mask;
-  __u32 flags;
-  __u32 fd;
-  __u32 unused;
+struct ion_phys_addr {
+  unsigned int dma_fd;
+  unsigned long phys_addr;
 };
-#define MAX_HEAP_NAME 32
-struct ion_heap_data {
-  char name[MAX_HEAP_NAME];
-  __u32 type;
-  __u32 heap_id;
-  __u32 reserved0;
-  __u32 reserved1;
-  __u32 reserved2;
+#define RCAR_ION_MAGIC 'R'
+#define RCAR_ION_CUSTOM _IOWR(RCAR_ION_MAGIC, 6, struct ion_custom_data)
+enum RCAR_CUSTOM_COMMANDS {
+  RCAR_GET_PHYS_ADDR = 1,
 };
-struct ion_heap_query {
-  __u32 cnt;
-  __u32 reserved0;
-  __u64 heaps;
-  __u32 reserved1;
-  __u32 reserved2;
-};
-#define ION_IOC_MAGIC 'I'
-#define ION_IOC_ALLOC _IOWR(ION_IOC_MAGIC, 0, struct ion_allocation_data)
-#define ION_IOC_HEAP_QUERY _IOWR(ION_IOC_MAGIC, 8, struct ion_heap_query)
 #endif
