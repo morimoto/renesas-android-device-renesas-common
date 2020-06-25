0917b7d162736712c7ab1959f607373bade84b48
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
2020-06-25 17:52:20 +0300

    build/car.mk: Added missing CompanionDeviceSupport package
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
    Change-Id: Icd963d5cb0d7b90ce384eed4e0819f8e2b8ff3d8

--- build/car.mk
+++ build/car.mk
@@ -22,7 +22,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += packages/services/Car/car_product/sepolicy/pr
 PRODUCT_PACKAGES += \
     Bluetooth \
     CarDeveloperOptions \
-    Contacts \
+    CompanionDeviceSupport \
     OneTimeInitializer \
     Provision \
     SystemUpdater \
