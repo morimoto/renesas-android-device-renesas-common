38ec0ea5dfec286d47eee6490fe94dce46b2928f
Oleksandr Volkov <oleksandr.v.volkov@globallogic.com>
2020-11-19 09:45:43 +0200

    Fix DRM clearkey service initialization
    Changed DRM clearkey service version from 1.2 to 1.3, because on
    DRM HAL initialization process version 1.3 is needed
    and it couldn't be found.
    
    Jira-Id: ADM-5236
    Signed-off-by: Oleksandr Volkov <oleksandr.v.volkov@globallogic.com>
    Change-Id: I18f4619bd79a91d5f8ae7764ca94ad59a037b7dc

--- DeviceCommon.mk
+++ DeviceCommon.mk
@@ -329,7 +329,7 @@ RODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.3-service.clearkey \
 
 # Generic HALs
 PRODUCT_PACKAGES += \
